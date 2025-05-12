4fc825193efe2ab7b18b4d59f24b8104df8210fa
Prairie Dogg <prairied0gg76@gmail.com>
2025-05-12 19:28:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Module build failed (from ./node_modules/next/dist/build/webpack/loaders/next-swc-loader.js):
    > Error:   x Unexpected eof
    >     ,-[/home/user/studio/src/components/envelopes/envelope-summary-list.tsx:83:1]
    >  80 |     </ScrollArea>
    >  81 |   );
    >  82 | }
    >  83 | ```
    >     `----
    >
    >
    > Caused by:
    >     Syntax Error

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-// Changed import path for ThemeProviderProps
+// Correct the import path for ThemeProviderProps
 import type { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
