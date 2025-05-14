12ffae38e5dd778e2a62c0a5c0872286db6689fd
Prairie Dogg <prairied0gg76@gmail.com>
2025-05-14 00:55:41 +0000

    remove measurementId from Next.js app's code

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Removed this line
 };
 
 let app: FirebaseApp;
@@ -33,3 +33,4 @@ const db = getFirestore(app);
 // }
 
 export { db, app };
+
